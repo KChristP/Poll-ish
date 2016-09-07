a78926a0c2d270f81b9d4c5c11d5d1676005b1c4
keenan <keenanphillips@gmail.com>
2016-09-07 14:08:45 -0700

    styled the vote form

--- frontend/components/vote_form.jsx
+++ frontend/components/vote_form.jsx
@@ -22,19 +22,26 @@ class VoteForm extends React.Component {
     let question = ""
     let answerChoices = "This user does not currently have any live polls"
     if(this.props.poll){
-      question = (<h1>{this.props.poll.question.body}</h1>);
+      question = (<h1 className="vote-form-question">{this.props.poll.question.body}</h1>);
       answerChoices = this.props.poll.question.answers.map((answer) => (
-        <label key={answer.body + answer.id}>{answer.body}
-          <input type="radio" name={this.props.poll.id} value={answer.id}/>
+        <label htmlFor={answer.body} key={answer.body + answer.id} className="vote-form-input-box">
+          <input
+            type="radio"
+            id={answer.body}
+            className="vote-form-radio"
+            name={this.props.poll.id}
+            value={answer.id}
+            key={answer.body}/>
+          {answer.body}
         </label>
       ));
-      answerChoices.push(<input type="submit" value="Submit Vote!"/>)
+      answerChoices.push(<input type="submit" value="Submit Vote!" className="vote-form-submit" key="submit"/>)
     }
 
     return(
       <div className="vote-form-box">
-        <form onSubmit={this.handleSubmit}>
-          {question}
+        {question}
+        <form onSubmit={this.handleSubmit} className="vote-form-itself">
           {answerChoices}
         </form>
       </div>
